9dd198ebe7dbcd84ef4686d1ca2f551e943f0f5e
leeroywking <leeroywking@gmail.com>
2019-07-09 21:37:05 -0700

    finished scaffolding

---
+++ models-singular/categories-schema.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+// What fields and constraints do we want?
+const categories = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: false },
+});
+
+// Do we need to run any lifecycle hooks/middleware?
+
+module.exports = mongoose.model('categories ', categories);
